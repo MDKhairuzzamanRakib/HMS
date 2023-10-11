b1b51ee70e5ebfa6448d44eefa357764ae9977a8
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2023-10-12 01:58:38 +0600

    Department Section Completed

--- Master Details/Controllers/DepartmentController.cs
+++ Master Details/Controllers/DepartmentController.cs
@@ -14,5 +14,73 @@ namespace Master_Details.Controllers
         {
             return View(db.Departments.ToList());
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(department);
+                db.SaveChanges();
+                return PartialView("_success");
+            }
+            return PartialView("_error");
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+        [HttpPost]
+        public ActionResult Edit(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(department).State=System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return PartialView("_success");
+            }
+            return PartialView("_error");
+        }
+
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Department department = db.Departments.Find(id);
+            db.Departments.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- Master Details/Models/Department.cs
+++ Master Details/Models/Department.cs
@@ -11,7 +11,8 @@ namespace Master_Details.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Department
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,7 +22,9 @@ namespace Master_Details.Models
         }
     
         public int DepartmentId { get; set; }
+        [Required]
         public string DepartmentName { get; set; }
+        [Required]
         public string DoctorName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
