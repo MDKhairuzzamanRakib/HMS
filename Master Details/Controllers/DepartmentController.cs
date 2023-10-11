cdb8e827bf1f7d65d71b6858e7d8a05dbfbe3d76
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2023-10-11 23:42:38 +0600

    Department Updated

---
+++ Master Details/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using Master_Details.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Master_Details.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        AppointDBEntities db = new AppointDBEntities();
+        public ActionResult Index()
+        {
+            return View(db.Departments.ToList());
+        }
+    }
+}
