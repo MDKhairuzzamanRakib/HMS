032b81d6e1059305045fdfcb9432888cd3b37739
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2023-10-12 12:19:16 +0600

    Appointment Table

---
+++ Master Details/Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+﻿using Master_Details.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Master_Details.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private AppointDBEntities db = new AppointDBEntities();
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            var appointments = db.Patients.Include(x=>x.Appointments.Select(y=>y.Department)).OrderByDescending(x=>x.PatientId).ToList();
+            return View(appointments);
+        }
+    }
+}
